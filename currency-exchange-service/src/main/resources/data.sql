835cfec984357d02b2e20903ec220ab1fdc9e9ca
w2cluster181@iiht.tech <w2cluster181@iiht.tech>
2020-05-21 21:51:53 +0530

    initial commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_factor,port)
+values(1001L,'USD','INR',75,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_factor,port)
+values(1002L,'AUD','INR',45,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_factor,port)
+values(1003L,'EUR','INR',86,0);
